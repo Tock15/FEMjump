b8e13d59fd03f2bd2f69e0dc46bf97c0474efd65
Koses Suvarnasuddhi <koses.su@gmail.com>
2025-03-29 16:13:42 +0700

    Added a character inside a scene which is inside a view

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    //Create a scene (area to display shit)
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    //Create Rectangle
+    QGraphicsPixmapItem * player = new QGraphicsPixmapItem();
+    player->setPixmap(QPixmap(":/img/astolfo.png").scaled(50,50));
+
+
+    scene->addItem(player);
+
+    //Create a view (window)
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
+
+
+    // MainWindow w;
+    // w.show();
     return a.exec();
 }
