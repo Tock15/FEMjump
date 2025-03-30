6a5f987fa7febb5f48d50a57b55373cd276f484b
Koses Suvarnasuddhi <koses.su@gmail.com>
2025-03-30 16:13:20 +0700

    THERES GRAVITY, player class now inherits from qObject so it can be connected to timer, still need to implement sideway jumping

--- Player.cpp
+++ Player.cpp
@@ -2,9 +2,12 @@
 #include <QDebug>
 #include <qevent.h>
 
-Player::Player()
+Player::Player(): velocityY(0), isJumping(false)
 {
     setPixmap(QPixmap(":/img/astolfo.png").scaled(50, 50, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Player::updatePosition);
+    timer->start(16); // Runs every 16ms (~60 FPS)
 }
 
 void Player::goRight()
@@ -17,7 +20,23 @@ void Player::goLeft()
     setPos(x()-5,y());
 }
 
-void Player::Jump()
+void Player::jump()
 {
+    if (!isJumping) {  // Prevent double jumps
+        velocityY = -15;
+        isJumping = true;
+    }
+}
+void Player::applyGravity(){
+    velocityY += 1;  // Simulate gravity
+    setPos(x(), y() + velocityY);
+}
+void Player::updatePosition() {
+    applyGravity();
 
+    if (y() > 600) {
+        setPos(x(), 600);
+        velocityY = 0;
+        isJumping = false;  // Allow jumping again
+    }
 }
