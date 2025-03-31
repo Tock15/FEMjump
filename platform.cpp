3798440b55dd04bae410566d4567b78801e98973
Noxy 3 <thatguynoxxie@gmail.com>
2025-03-31 19:09:24 +0700

    added platform

---
+++ platform.cpp
@@ -0,0 +1,8 @@
+#include "platform.h"
+
+Platform::Platform(qreal x, qreal y, qreal width, qreal height, QGraphicsItem *parent)
+    : QGraphicsRectItem(x, y, width, height, parent)
+{
+    //stone grey
+    setBrush(QBrush(Qt::gray));
+}
---
+++ platform.h
@@ -0,0 +1,15 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include <QObject>
+#include <QGraphicsRectItem>
+#include <QBrush>
+
+class Platform : public QObject, public QGraphicsRectItem
+{
+    Q_OBJECT
+public:
+    explicit Platform(qreal x, qreal y, qreal width, qreal height, QGraphicsItem *parent = nullptr);
+};
+
+#endif // PLATFORM_H
